0757aaac24441cdedf3e3ce0a79298d119f844d2
JesseAger <agerjesse6@gmail.com>
2024-02-23 14:21:24 -0500

    sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min_idx;
+
+	if (!array)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+		if (i != min_idx)
+		{
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+		}
+	}
+}
+
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
